64518fd73642a12f36bac65583d2b372024f058a
cronth <48060258+cronth@users.noreply.github.com>
2020-09-17 13:09:35 +0200

    Daten werden per POST request von Android Gerät in MySQL DB übertragen. Rohbau; keine Sicherheitsmaßnahmen jeglicher Art.

--- app/src/main/java/de/cronth/volleypostrequest/MainActivity.java
+++ app/src/main/java/de/cronth/volleypostrequest/MainActivity.java
@@ -3,19 +3,38 @@ package de.cronth.volleypostrequest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText txtName;
-    EditText txtPassword;
+    EditText txtAge;
     Button btnSend;
+    TextView txtView;
     String name;
-    String password;
+    String age;
     SendData sendData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtName = findViewById(R.id.txt_name);
-        txtPassword = findViewById(R.id.txt_password);
+        txtAge = findViewById(R.id.txt_age);
         btnSend = findViewById(R.id.btn_send);
 
         btnSend.setOnClickListener(new View.OnClickListener() {
@@ -31,16 +50,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 name = txtName.getText().toString();
-                password = txtPassword.getText().toString();
-                if (name.length() == 0 || password.length() == 0) {
-                    Toast.makeText(MainActivity.this, "Bitte Name und PW eingeben", Toast.LENGTH_SHORT).show();
-                } else {
-                    sendData = new SendData(name, password);
-                    if (sendData.sendToPC()) {
-                        Toast.makeText(MainActivity.this, "Alles klar", Toast.LENGTH_SHORT).show();
-                    }
-                }
+                age = txtAge.getText().toString();
+
+                callvolly(name, age);
             }
-        });
+        });//Ende btnSend.setOnClickListener
+    }
+
+    public void callvolly(final String name, final String age){
+
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(this);
+        String url = "http://192.168.178.49/volley/insert.php";
+        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //This code is executed if the server responds, whether or not the response contains data.
+                //The String 'response' contains the server's response.
+            }
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                txtView.setText("Sumtn wrong");
+                //This code is executed if there is an error.
+            }
+        }) {
+            protected Map<String, String> getParams() {
+                Map<String, String> MyData = new HashMap<String, String>();
+                MyData.put("name", name);
+                MyData.put("age", age);
+                return MyData;
+            }
+        };
+
+
+        MyRequestQueue.add(MyStringRequest);
     }
 }
--- app/src/main/java/de/cronth/volleypostrequest/SendData.java
+++ app/src/main/java/de/cronth/volleypostrequest/SendData.java
@@ -1,21 +1,37 @@
 package de.cronth.volleypostrequest;
 
-public class SendData {
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SendData extends AppCompatActivity {
 
     String name;
-    String password;
+    String age;
 
-    public SendData(String name, String password) {
+    public SendData(String name, String age) {
         this.name = name;
-        this.password = password;
+        this.age = age;
     }
+
     private SendData() {
     }
 
     public boolean sendToPC() {
 
 
+
         return true;
+                }
 
     }
-}
